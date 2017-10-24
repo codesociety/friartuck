ac3c218054a02ca662ccd77930c4c70c31ec6968
codesociety <clivens.laguerre@gmail.com>
2017-10-24 06:45:26 -0500

    cleanup

--- friartuck/quote_source.py
+++ friartuck/quote_source.py
@@ -49,7 +49,7 @@ class GoogleQuoteSource(QuoteSourceAbstract):
 
     def fetch_quotes(self, symbol, bar_count=1, frequency='1m', field=None):
         if frequency not in self.allowed_history_frequency:
-            log.warn("frequency used (%s) is not allowed, the allowable list includes (%s)" % (frequency, self.allowed_history_frequency))
+            log.warning("frequency used (%s) is not allowed, the allowable list includes (%s)" % (frequency, self.allowed_history_frequency))
             return [];
 
         interval = 60
@@ -147,7 +147,7 @@ def _load_quotes(symbol, frequency, interval, period_factor, period, bar_count, 
                 bars = bars.append(bar)
 
     if bars is None:
-        log.warn("Unexpected, could not retrieve quote for security (%s) " % symbol)
+        log.warning("Unexpected, could not retrieve quote for security (%s) " % symbol)
         return None
 
     bars = bars.tail(bar_count)
